0276f524b0d20021fcd3dcb54c7037cb2ecbc0e3
strajk- <7372902+strajk-@users.noreply.github.com>
2022-12-12 15:57:19 +0100

    Add files via upload

---
+++ ForceToPrivateNetwork/ForceToPrivateNetwork.ps1
@@ -0,0 +1,9 @@
+## Change NetWorkConnection Category to Private
+#Requires -RunasAdministrator
+
+Get-NetConnectionProfile |
+  Where{ $_.NetWorkCategory -ne 'Private'} |
+  ForEach {
+    $_
+    $_|Set-NetConnectionProfile -NetWorkCategory Private
+  }
