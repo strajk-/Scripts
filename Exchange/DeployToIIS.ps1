895d9b96dca363cad185f763c862dd5ef78bc89d
strajk- <7372902+strajk-@users.noreply.github.com>
2022-12-14 16:47:36 +0100

    Add files via upload

---
+++ Exchange/DeployToIIS.ps1
@@ -0,0 +1,19 @@
+param($result, [String] $SiteName, [String] $CertThumbprint)
+
+# Dependencies
+Import-Module Webadministration
+
+# In case CertifyTheWeb was used
+if($result){
+	$CertThumbprint = $result.ManagedItem.CertificateThumbprintHash;
+}
+
+if(!$SiteName -Or !$CertThumbprint){
+	Write-Output("Input SiteName and CertificateThumbprint as parameters to run this script")
+	Write-Output("Example: -SiteName comodo.com -CertThumbprint a909502dd82ae41433e6f83886b00d4277a32a7b")
+}else{
+	$site = Get-ChildItem -Path "IIS:\Sites" | where {( $_.Name -eq "($($SiteName))" )}
+	$binding = $site.Bindings.Collection | where {( $_.protocol -eq 'https')}
+	
+	$binding.AddSslCertificate($CertThumbprint, "my")
+}
